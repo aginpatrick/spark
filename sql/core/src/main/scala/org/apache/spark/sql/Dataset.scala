66f01da9debd14abdd3107ca3af96c88efb7f888
Li Jin <li.jin@twosigma.com>
2016-12-12 16:50:51 -0500

    Test suite prototyping for collectAsArrow
    Changed scope of arrow-tools dependency to test
    
    commented out lines to Integration.compareXX that are private to arrow
    
    closes #10

--- sql/core/src/main/scala/org/apache/spark/sql/Dataset.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/Dataset.scala
@@ -2526,8 +2526,8 @@ class Dataset[T] private[sql](
    * @since 2.2.0
    */
   @DeveloperApi
-  def collectAsArrow(): ArrowRecordBatch = {
-    val allocator = new RootAllocator(Long.MaxValue)
+  def collectAsArrow(
+      allocator: RootAllocator = new RootAllocator(Long.MaxValue)): ArrowRecordBatch = {
     withNewExecutionId {
       try {
         val collectedRows = queryExecution.executedPlan.executeCollect()
